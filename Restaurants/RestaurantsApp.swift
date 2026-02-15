93e4e53afaab9d7349a6bb07fb6a42887ccc660f
AyanAllahverdili <allahverdiliayan@gmail.com>
2026-02-15 10:43:02 +0400

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Ayan Allahverdili on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
